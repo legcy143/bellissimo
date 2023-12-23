5ff1039536eb88e02af200776e4e2cbce1f3c2e5
manish kumar <techno8427@gmail.com>
2023-12-23 14:59:15 +0530

    ??

--- src/screens/starter/SplashScreen.tsx
+++ src/screens/starter/SplashScreen.tsx
@@ -1,34 +1,22 @@
-<<<<<<< HEAD
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import TextUi from '../../lib/ui/TextUi';
 import {FaIcons} from '../../lib/ui/IconsUi';
+import ButtonUi from '../../lib/ui/ButtonUi';
 
-const SplashScreen = () => {
+const SplashScreen = ({navigation}: any) => {
   return (
     <View>
-      <FaIcons icons="house" />
-      <TextUi>splash only</TextUi>
+      <ButtonUi
+        label="home"
+        onPress={() => {
+          navigation.navigate('BottomNavigation');
+        }}
+      />
+      <Text>SplashScreen</Text>
     </View>
   );
 };
-=======
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import ButtonUi from '../../lib/ui/ButtonUi'
-
-const SplashScreen = ({ navigation }: any) => {
-    return (
-        <View>
-            <ButtonUi label='home' onPress={() => {
-                navigation.navigate("BottomNavigation")
-            }} />
-            <Text>SplashScreen</Text>
-        </View>
-    )
-}
->>>>>>> 1c05cd0ad52f3822fe6c2ebbe87a91b2bc227b4d
-
 export default SplashScreen;
 
 const styles = StyleSheet.create({});
